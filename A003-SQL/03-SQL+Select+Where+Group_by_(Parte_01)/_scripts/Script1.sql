7671990780db5b20eed4235237c11a2b224cefcd
Altamiro B. Cardoso de Sousa <87079817+altamirosousa@users.noreply.github.com>
2022-01-12 01:18:17 -0300

    Aulas "03-SQL+Select+Where+Group_by_(Parte_01)" e "04-SQL+Select+Where+Group_by_(Parte_02)"

---
+++ A003-SQL/03-SQL+Select+Where+Group_by_(Parte_01)/_scripts/Script1.sql
@@ -0,0 +1,17 @@
+
+--CREATE TABLE BILLBOARD
+create table public."Billboard" (
+	"date" date null,
+	"rank" int4 null,
+	song varchar(300) null,
+	artist varchar(300) null,
+	"last-week" int4 null,
+	"peak-rank" int4 null,
+	"weeks-on-board" int4 null
+);
+
+select count(*) as quantidade
+from public."Billboard"
+limit 100
+
+
